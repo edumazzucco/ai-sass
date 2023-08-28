bbdbe8ccc24b35aa285b8904cc4aebd3714f99d6
edumazzucco <edumazzucco@hotmail.com>
2023-08-28 19:55:02 -0300

    feat: started sidebar

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full relative">
+      <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <div>
+          <p className="text-white text-xl">Sidebar</p>
+        </div>
+      </div>
+      <main className="md:pl-72">{children}</main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
