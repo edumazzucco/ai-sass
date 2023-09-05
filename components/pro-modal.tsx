159f2e68d5b09b39bda81c09d02fa43e72da3e98
edumazzucco <edumazzucco@hotmail.com>
2023-09-05 16:59:37 -0300

    feat: stripe subscription webhook

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -22,6 +22,8 @@ import {
 import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
+import axios from "axios";
+import { useState } from "react";
 
 const tools = [
   {
@@ -58,6 +60,19 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = useProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.error(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -89,7 +104,12 @@ export const ProModal = () => {
           ))}
         </DialogDescription>
         <DialogFooter>
-          <Button size="lg" variant="premium" className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size="lg"
+            variant="premium"
+            className="w-full"
+          >
             Upgrade <Zap className="w-4 h-4 ml-2" />{" "}
           </Button>
         </DialogFooter>
